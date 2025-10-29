a90bb361cf8686366a60bc40a160aca3bcc86812
Kai <kai.conragan@gmail.com>
2025-10-28 18:38:23 -0700

    nvim: formatting + md/mdx patch + mason rename

--- common/nvim/lua/config/options.lua
+++ common/nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- Treat .mdx as "markdown.mdx"
+vim.filetype.add({
+  extension = { mdx = "markdown.mdx" },
+})
--- common/nvim/lua/plugins/formatting.lua
+++ common/nvim/lua/plugins/formatting.lua
@@ -1,5 +1,4 @@
 return {
-  -- Auto-format on save with Conform
   {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
@@ -19,7 +18,7 @@ return {
         sh = { "shfmt" },
         bash = { "shfmt" },
 
-        -- Web / Astro / MDX / Markdown
+        -- Web / Astro / Markdown / MDX
         javascript = { "prettierd", "prettier" },
         typescript = { "prettierd", "prettier" },
         javascriptreact = { "prettierd", "prettier" },
@@ -33,42 +32,29 @@ return {
         json = { "prettierd", "prettier" },
         jsonc = { "prettierd", "prettier" },
         yaml = { "prettierd", "prettier" },
+
+        -- The important bits for you:
         markdown = { "prettierd", "prettier" },
         mdx = { "prettierd", "prettier" },
-
-        -- Extras
-        toml = { "taplo" },
-        terraform = { "terraform_fmt" },
-        c = { "clang_format" },
-        cpp = { "clang_format" },
-        go = { "gofumpt", "goimports" },
-        rust = { "rustfmt" },
-      },
-    },
-  },
-
-  -- Lightweight lint on save / insert-leave
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufWritePost", "InsertLeave", "BufReadPost", "BufNewFile" },
-    opts = {
-      linters_by_ft = {
-        python = { "ruff" },
-        sh = { "shellcheck" },
-        bash = { "shellcheck" },
-        markdown = { "markdownlint" },
-        dockerfile = { "hadolint" },
-        -- For JS/TS/MDX you can use eslint-lsp instead of nvim-lint.
+        ["markdown.mdx"] = { "prettierd", "prettier" },
+        ["markdown.gfm"] = { "prettierd", "prettier" }, -- just in case
       },
     },
     config = function(_, opts)
-      local lint = require("lint")
-      lint.linters_by_ft = opts.linters_by_ft or {}
-      vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
-        callback = function()
-          lint.try_lint()
+      local conform = require("conform")
+      conform.setup(opts)
+
+      -- Explicit save hooks so markdown/mdx always format
+      local aug = vim.api.nvim_create_augroup("ConformMarkdownOnSave", { clear = true })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = aug,
+        pattern = { "*.md", "*.mdx" },
+        callback = function(args)
+          conform.format({ bufnr = args.buf, lsp_fallback = true, timeout_ms = 1500 })
         end,
       })
     end,
   },
+
+  -- keep your nvim-lint block below unchanged
 }
