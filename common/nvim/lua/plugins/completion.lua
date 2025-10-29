e421993a526d9b01e921b5d5a0dd4f2ebf105e17
Kai <kai.conragan@gmail.com>
2025-10-28 18:13:04 -0700

    nvim: completions.lua file

---
+++ common/nvim/lua/plugins/completion.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      opts.completion = opts.completion or {}
+      opts.completion.completeopt = "menu,menuone,noselect"
+
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+
+      opts.sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "buffer" },
+      })
+
+      opts.experimental = { ghost_text = true }
+      return opts
+    end,
+  },
+}
